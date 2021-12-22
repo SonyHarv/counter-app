a1d41eea9ab13a046bc3911563837a4ed1cc4201
Erikson TF <sonyharv9@gmail.com>
2021-12-22 09:58:43 -0800

    Add new component Counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Hola Mundo desde Counter!!!</h1>
+    <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  </div>
+</template>
+<script>
+// import Counter from "@/components/Counter.vue";
+
+export default {
+  name: "Counter",
+  data() {
+    return {
+      counter: 5,
+    };
+  },
+  methods: {
+    getSquareValue() {
+      // console.log("getSquareValue");
+      return this.counter * this.counter;
+    },
+  },
+  computed: {
+    squareCounter() {
+      // console.log("computed squareCounter");
+      return this.counter * this.counter;
+    },
+  },
+};
+</script>
+<style>
+h1 {
+  color: green;
+}
+</style>
