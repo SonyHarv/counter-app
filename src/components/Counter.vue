867a4a43040b96fd810c128f51c274f6d4c315cd
Erikson TF <sonyharv9@gmail.com>
2021-12-23 15:09:40 -0800

    Add method increase, decrease, buttons y props

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1>Hola Mundo desde Counter!!!</h1>
+    <h2>{{ customTitle }}</h2>
     <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+  </div>
+  <div>
+    <button @click="incrementValue">+1</button>
+    <button @click="decrementValue">-1</button>
   </div>
 </template>
 <script>
@@ -9,6 +13,8 @@
 
 export default {
   name: "Counter",
+
+  props: ["title"],
   data() {
     return {
       counter: 5,
@@ -19,17 +25,44 @@ export default {
       // console.log("getSquareValue");
       return this.counter * this.counter;
     },
+    incrementValue() {
+      // console.log(this.incrementValue);
+      this.counter++;
+    },
+    decrementValue() {
+      this.counter--;
+    },
   },
   computed: {
     squareCounter() {
       // console.log("computed squareCounter");
       return this.counter * this.counter;
     },
+    customTitle() {
+      // return this.title ? this.title : "Counter";
+      return this.title || "Counter";
+    },
   },
 };
 </script>
+
 <style>
 h1 {
   color: green;
+}
+button {
+  color: white;
+  padding-inline: 0.8rem;
+  padding-block: 0.3rem;
+  margin-inline-end: 0.7rem;
+  border-radius: 5px;
+  cursor: pointer;
+  border: none;
+  background: #4f9a6f;
+  transition: 0.4s ease-in-out;
+}
+button:hover {
+  background: #53b47d;
+  transition: 0.4s ease-in-out;
 }
 </style>
